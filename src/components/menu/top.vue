0322458dc28499aa1239006862d0b94bee0a8e4d
blackchestnut <alexander.kalinichev@gmail.com>
2020-09-11 02:37:35 +0300

    Add background for left sidebar

--- src/components/menu/top.vue
+++ src/components/menu/top.vue
@@ -1,6 +1,10 @@
 <template>
   <div class='menu'>
-    <div class='left-button' />
+    <div class='left-button' @click='toggle' />
+    <div class='left-menu-backdrop' @click='toggle' v-if='isExpanded' />
+    <transition name='slide'>
+      <div v-if='isExpanded' class='left-menu' />
+    </transition>
     <div v-if='title' class='title'>
       {{ title }}
     </div>
@@ -15,6 +19,9 @@ export default {
   props: {
     title: { type: String, required: false, default: null },
   },
+  data: () => ({
+    isExpanded: false
+  }),
   computed: {
     boards: get('user/boards'),
     isFull() {
@@ -22,6 +29,9 @@ export default {
     }
   },
   methods: {
+    toggle() {
+      this.isExpanded = !this.isExpanded;
+    }
   }
 };
 </script>
@@ -51,10 +61,40 @@ export default {
   position: absolute
   top: 4px
   width: 28px
+  z-index: 4
 
   &:hover
     background-color: #5C6BC0
 
   &:active
     background-color: #303F9F
+
+.left-menu
+  background-color: #283593
+  height: 100vh
+  left: 0
+  position: fixed
+  top: 0
+  width: 320px
+  z-index: 3
+  // transition: transform 30ms ease-in-out 0ms
+
+.left-menu-backdrop
+  z-index: 2
+  // background-color: rgba(0,0,0,.1)
+  width: 100vw
+  height: 100vh
+  position: fixed
+  top: 0
+  left: 0
+
+.slide-enter-active,
+.slide-leave-active
+  // transform: translateX(-100%)
+  // transition: transform 1.2s ease
+
+.slide-enter,
+.slide-leave-to
+  transform: translateX(-100%)
+  transition: all 200ms ease 0ms
 </style>
