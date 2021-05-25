36690c3fbfce0c62e3ff732ee909637a6f475b1f
blackchestnut <alexander.kalinichev@gmail.com>
2021-05-25 21:28:21 +0300

    Update styles for invites view

--- src/views/invite.vue
+++ src/views/invite.vue
@@ -1,21 +1,52 @@
 <template>
-  <div>
-    <div>
-      Signed in: {{ isSignedIn }}
+  <div class='container'>
+    <h1>Agile Season</h1>
+    <h2>{{ subtitle }}</h2>
+    <Loader v-if='isLoading' />
+
+    <div v-if='isGoHomeNote'>
+      <p>Please Sign in before using invitation link</p>
+      <Button
+        :href='/#/'
+        text='Sign in'
+      />
     </div>
-    {{ token }}
+
+    <div class='dev-notes'>
+      Signed in: {{ isSignedIn }}
+      <br />
+      {{ token }}
+    </div>
   </div>
 </template>
 
 <script>
 // http://localhost:8080/#/i/670299745d0a0ee21f489f7f7df8c86d
+import Button from '@/components/buttons/button.vue'
+import Loader from '@/components/loader';
 import { get, call } from 'vuex-pathify';
 
 export default {
-  data: () => ({}),
+  components: {
+    Button,
+    Loader
+  },
+  data: () => ({
+    isLoading: true,
+    boardName: undefined
+  }),
   computed: {
     isSignedIn: get('user/isSignedIn'),
-    token() { return this.$route.params.token; }
+    token() { return this.$route.params.token; },
+    subtitle() {
+      if (this.isLoading) { return ''; }
+      if (this.boardName == null) { return 'Invite is not found'; }
+
+      return 'Invite';
+    },
+    isGoHomeNote() {
+      return !this.isLoading && !this.isSignedIn;
+    }
   },
   methods: {
     ...call([
@@ -24,3 +55,10 @@ export default {
   }
 }
 </script>
+
+<style scoped lang='sass'>
+.container
+  max-width: 1000px
+  margin: 40px auto
+  text-align: center
+</style>
