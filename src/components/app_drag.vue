a52ddb35304f3808db00415157916da87696666c
blackchestnut <alexander.kalinichev@gmail.com>
2021-05-31 22:38:08 +0300

    code cleanup

--- src/components/app_drag.vue
+++ src/components/app_drag.vue
@@ -11,6 +11,15 @@
 </template>
 
 <script>
+// https://www.tutorialspoint.com/html5/html5_drag_drop.htm
+// 1. dragstart
+// 2. dragenter
+// 3. dragover
+// 4. dragleave
+// 5. drag
+// 6. drop
+// 7. dragend
+
 export default {
   props: {
     transferData: {
