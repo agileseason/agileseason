9f6cc8010464a6be76c67de79b2c84ca6b098b8b
blackchestnut <alexander.kalinichev@gmail.com>
2021-09-06 21:14:57 +0300

    misc

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -116,17 +116,18 @@ export default {
   //   console.log(process.env.VUE_APP_GITHUB_CLIENT_SECRET)
   // }
 }
+
+// .container
+//   @media screen and (min-width: 1023px)
+//     max-width: 1200px
+//   @media screen and (max-width: 1023px)
+//     max-width: 1000px
 </script>
 
 <style scoped lang='sass'>
 .container
   margin: 0 auto
   padding: 40px 10px
-
-  @media screen and (min-width: 1023px)
-    max-width: 1200px
-  @media screen and (max-width: 1023px)
-    max-width: 1000px
 
 .signin
   min-width: 130px
