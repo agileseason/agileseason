23b3875cc5cb18c3811b4738cd308a05a2a6d436
blackchestnut <alexander.kalinichev@gmail.com>
2021-09-13 23:11:04 +0300

    misc

--- src/main.js
+++ src/main.js
@@ -1,22 +1,24 @@
 import App from './app';
 import { createApp } from 'vue';
-import { Integrations } from '@sentry/tracing';
-import * as Sentry from '@sentry/vue';
 import router from './router';
 import store from './store';
 
 const app = createApp(App);
 
-Sentry.init({
-  app,
-  dsn: 'https://343692c487dd4c37bc0a15957448f6a2@o242649.ingest.sentry.io/5957729',
-  integrations: [
-    new Integrations.BrowserTracing({
-      routingInstrumentation: Sentry.vueRouterInstrumentation(router),
-      tracingOrigins: ['localhost', 'agileseason.com', /^\//],
-    }),
-  ],
-  tracesSampleRate: 1.0,
-});
+import { Integrations } from '@sentry/tracing';
+import * as Sentry from '@sentry/vue';
+if (process.env.NODE_ENV === 'production') {
+  Sentry.init({
+    app,
+    dsn: 'https://343692c487dd4c37bc0a15957448f6a2@o242649.ingest.sentry.io/5957729',
+    integrations: [
+      new Integrations.BrowserTracing({
+        routingInstrumentation: Sentry.vueRouterInstrumentation(router),
+        tracingOrigins: ['localhost', 'agileseason.com', /^\//],
+      }),
+    ],
+    tracesSampleRate: 1.0,
+  });
+}
 
 app.use(store).use(router).mount('#app');
