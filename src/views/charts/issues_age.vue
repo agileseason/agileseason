3c3835fb81b2d6fcfba11fcb780ccd2f7be4b18b
blackchestnut <alexander.kalinichev@gmail.com>
2022-02-12 19:31:14 +0300

    Update chart example

--- src/views/charts/issues_age.vue
+++ src/views/charts/issues_age.vue
@@ -21,25 +21,26 @@ export default {
   },
   data: () => ({
     series: [{
-      name: 'Net Profit',
-      data: [44, 55, 57, 56, 61, 58, 63, 60, 66]
-    }, {
-      name: 'Revenue',
-      data: [76, 85, 101, 98, 87, 105, 91, 114, 94]
-    }, {
-      name: 'Free Cash Flow',
-      data: [35, 41, 36, 26, 45, 48, 52, 53, 41]
+      name: 'Age',
+      data: [200, 199, 150, 90, 50, 40, 30, 26, 20, 5, 4, 1, 1]
+    // }, {
+    //   name: 'Revenue',
+    //   data: [76, 85, 101, 98, 87, 105, 91, 114, 94]
+    // }, {
+    //   name: 'Free Cash Flow',
+    //   data: [35, 41, 36, 26, 45, 48, 52, 53, 41]
     }],
     chartOptions: {
       chart: {
         type: 'bar',
         height: 350,
-        toolbar: { show: false }
+        toolbar: { show: false },
+        animations: { speed: 400 }
       },
       plotOptions: {
         bar: {
           horizontal: false,
-          columnWidth: '55%',
+          columnWidth: '85%',
           endingShape: 'rounded'
         },
       },
@@ -52,12 +53,10 @@ export default {
         colors: ['transparent']
       },
       xaxis: {
-        categories: ['Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct'],
+        categories: ['#1', '#3', '#10', '#19', '#20', '#22', '#24', '#26', '#28', '#30', '#40', '#41', '#44'],
       },
       yaxis: {
-        title: {
-          text: '$ (thousands)'
-        }
+        title: { text: 'Days' }
       },
       fill: {
         opacity: 1
@@ -65,7 +64,7 @@ export default {
       tooltip: {
         y: {
           formatter: function (val) {
-            return "$ " + val + " thousands"
+            return `${val} days`;
           }
         }
       }
