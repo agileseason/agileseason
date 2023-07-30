1fb96205b7fd0b05dc1227a5dcb61a211e4bf2a6
blackchestnut <alexander.kalinichev@gmail.com>
2023-07-30 19:54:56 +0300

    Re-set cookies in getter function #168

--- src/utils/cookie_store.js
+++ src/utils/cookie_store.js
@@ -2,13 +2,25 @@ import cookies from 'browser-cookies';
 
 export default {
   set(name, key, value) {
-    cookies.set(`${name}_${key}`, JSON.stringify(value), { expires: 365 });
+    cookies.set(
+      `${name}_${key}`,
+      JSON.stringify(value),
+      { expires: 365 }
+    );
   },
   get(name, key, defaultValue) {
-    const value = cookies.get(`${name}_${key}`);
-
-    return value ?
-      JSON.parse(value) :
-      defaultValue;
+    try {
+      const value = cookies.get(`${name}_${key}`);
+      // Переустанавливаем куки заново, чтобы обойти ограничение в 7 дней
+      // для Safari и Brave.
+      if (value) {
+        const parsedValue = JSON.parse(value);
+        this.set(name, key, parsedValue);
+        return parsedValue;
+      }
+      return defaultValue;
+    } catch  {
+      return defaultValue;
+    }
   }
 };
